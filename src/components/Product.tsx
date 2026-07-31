3cb454abe44f3fec3515b09f6bad821cc7786a21
victor-jedamanov <vjedamanov@gmail.com>
2026-07-30 21:24:35 -0400

    Now imports types

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -9,19 +9,7 @@ import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import './Product.css';
 
-interface productInfo {
-  asin: string;
-  product_title: string;
-  product_price: string;
-  product_original_price: string;
-  product_star_rating: string;
-  product_num_ratings: string;
-  product_image: string;
-  is_prime: boolean;
-  amount_sold: string;
-  delivery_info: string;
-  productStatus: string;
-}
+import type { productInfo } from '../types/productInfo';
 
 const options = [
   'Batteries',
